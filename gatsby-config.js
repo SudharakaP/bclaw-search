cccefcabcfdd4269b2173bae48c2a0d0b1debd0f
Sudharaka <sudharakap@mun.ca>
2024-10-08 11:23:18 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bclaws-search`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
